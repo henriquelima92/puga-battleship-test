d0c7257e76f52da0ab464a65a32a0344c9c70923
henriquelima92 <henriquedelima92@hotmail.com>
2020-10-27 17:27:28 -0300

    created the main menu panel with start, edit ship and quit game buttons.

---
+++ Assets/Scripts/UI/MainMenuPanel.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuPanel : MonoBehaviour
+{
+    public void StartGame()
+    {
+        InGameCanvas.OnGameStartEvent?.Invoke();
+    }
+    public void EditShip()
+    {
+
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
